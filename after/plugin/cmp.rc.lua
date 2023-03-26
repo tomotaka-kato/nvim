58e75cd696ac833d78b7b96b301ada3ca97995c7
Kato Tomotaka <tomotaka.k612@gmail.com>
2023-03-26 18:23:51 +0900

    duplicatedとなっているコマンドを使わないように変更

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -5,7 +5,7 @@ if (not status) then
 end
 local lspkind = require 'lspkind'
 
-capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
