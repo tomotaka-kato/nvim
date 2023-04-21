28f7fa4207c758d56b6128e2c75b782d2693d540
Kato Tomotaka <tomotaka.katou.dw@fujifilm.com>
2023-04-21 01:12:23 +0000

    add which-key

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -141,6 +141,19 @@ packer.startup(function(use)
 
     use { 'editorconfig/editorconfig-vim' }
 
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+}
+
 end)
 
 vim.cmd([[colorscheme onedark]])
