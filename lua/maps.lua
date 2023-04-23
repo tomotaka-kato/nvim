afeab8989d0e5a5bd5438cb9bca8ef48305cc876
tomo <tomotaka.k612@gmail.com>
2023-04-24 02:28:43 +0900

    add open lazygit mapping

--- lua/maps.lua
+++ lua/maps.lua
@@ -19,4 +19,5 @@ keymap.set('t', '<C-t>', '<C-\\><C-n>', { noremap = true })
 keymap.set('n', '<C-t>s', '<cmd>botright new<CR><cmd>terminal<CR>', { noremap = true })
 keymap.set('n', '<C-t>v', '<cmd>rightbelow vnew<CR><cmd>terminal<CR>', { noremap = true })
 keymap.set('n', '<C-t>n', '<cmd>tabe<CR><cmd>terminal<CR>', { noremap = true })
+keymap.set('n', '<C-t>g', '<cmd>tabe<CR><cmd>terminal<CR>lazygit<CR>', { noremap = true })
 
