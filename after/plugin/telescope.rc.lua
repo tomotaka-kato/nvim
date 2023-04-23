dcd87def26e330db4238e81fe4bbf87316cb78b9
tomo <tomotaka.k612@gmail.com>
2023-04-24 02:37:20 +0900

    change keymap writing

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -5,8 +5,15 @@ if not status_telescope then
     return
 end
 
-vim.cmd [[nnoremap <C-p> <cmd>Telescope find_files<cr>]]
-vim.cmd [[nnoremap <leader>fg <cmd>Telescope live_grep<cr>]]
-vim.cmd [[nnoremap <leader>fb <cmd>Telescope buffers<cr>]]
-vim.cmd [[nnoremap <leader>fd <cmd>Telescope diagnostics<cr>]]
+vim.cmd [[autocmd User TelescopePreviewerLoaded setlocal wrap]]
+
+-- vim.cmd [[nnoremap <C-p> <cmd>Telescope find_files<cr>]]
+-- vim.cmd [[nnoremap <leader>fg <cmd>Telescope live_grep<cr>]]
+-- vim.cmd [[nnoremap <leader>fb <cmd>Telescope buffers<cr>]]
+-- vim.cmd [[nnoremap <leader>fd <cmd>Telescope diagnostics<cr>]]
+
+vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<CR>', { noremap = true })
 
