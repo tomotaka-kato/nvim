db8244523f7df132399365c5f7f6a14763aecece
Kato Tomotaka <tomotaka.katou.dw@fujifilm.com>
2023-04-25 07:25:45 +0000

    map変更

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -15,9 +15,3 @@ telescope.setup({
 })
 
 vim.cmd [[autocmd User TelescopePreviewerLoaded setlocal wrap]]
-
-vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files<CR>', { noremap = true })
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
-vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<CR>', { noremap = true })
-
--- lua/maps.lua
+++ lua/maps.lua
@@ -3,21 +3,26 @@ local keymap = vim.keymap
 -- leaderはspaceバーにする
 vim.g.mapleader = " "
 
--- window移動
-keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
-keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
-keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
-keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
+local noramap = { noremap = true }
 
--- Increment/Decriment
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+-- window移動
+keymap.set('n', '<C-j>', '<C-w>j', noramap)
+keymap.set('n', '<C-h>', '<C-w>h', noramap)
+keymap.set('n', '<C-k>', '<C-w>k', noramap)
+keymap.set('n', '<C-l>', '<C-w>l', noramap)
 
 -- ターミナルのインサートモードから抜けるコマンドを変更
-keymap.set('t', '<C-t>', '<C-\\><C-n>', { noremap = true })
+keymap.set('t', '<C-t>', '<C-\\><C-n>', noramap)
 -- ターミナルの起動コマンド
-keymap.set('n', '<C-t>s', '<cmd>botright new<CR><cmd>terminal<CR>', { noremap = true })
-keymap.set('n', '<C-t>v', '<cmd>rightbelow vnew<CR><cmd>terminal<CR>', { noremap = true })
-keymap.set('n', '<C-t>n', '<cmd>tabe<CR><cmd>terminal<CR>', { noremap = true })
-keymap.set('n', '<C-t>g', '<cmd>tabe<CR><cmd>terminal<CR>lazygit<CR>', { noremap = true })
+keymap.set('n', '<C-t>s', '<cmd>botright new<CR><cmd>terminal<CR>', noramap)
+keymap.set('n', '<C-t>v', '<cmd>rightbelow vnew<CR><cmd>terminal<CR>', noramap)
+keymap.set('n', '<C-t>n', '<cmd>tabe<CR><cmd>terminal<CR>', noramap)
+keymap.set('n', '<C-t>g', '<cmd>tabe<CR><cmd>terminal<CR>lazygit<CR>', noramap)
+
+-- Telescope
+keymap.set('n', '<C-p>', '<cmd>Telescope find_files<CR>', noramap)
+keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', noramap)
+keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', noramap)
+keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<CR>', noramap)
+
 
