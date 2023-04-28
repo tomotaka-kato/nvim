127d883659a82c5538b5153dd7f9ea7b941b2046
Kato Tomotaka <tomotaka.katou.dw@fujifilm.com>
2023-04-28 05:09:53 +0000

    modify

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,6 +51,26 @@ packer.startup(function(use)
     use('m-demare/hlargs.nvim') -- 引数で渡された変数に色をつける
     use('nvim-treesitter/nvim-treesitter-textobjects') -- テキストオブジェクトを追加
     use('David-Kunz/treesitter-unit')
+    -- 画面に収まらない関数名を表示する
+    -- デフォルトの設定だとちょっと見にくいので使わないかも。。。
+    -- use{
+    --     'nvim-treesitter/nvim-treesitter-context' ,
+    --     config = function()
+    --         require'treesitter-context'.setup{
+    --             enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    --             max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    --             min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+    --             line_numbers = true,
+    --             multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+    --             trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    --             mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+    --             -- Separator between context and content. Should be a single character string, like '-'.
+    --             -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    --             separator = nil,
+    --             zindex = 20, -- The Z-index of the context window
+    --         }
+    --     end
+    -- }
     -- [end] treesitter
 
     -- カッコの補完
