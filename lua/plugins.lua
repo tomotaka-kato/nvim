795ce14a83f8c16bd56df19b8abf4088e8fac1e2
tomo <tomotaka.k612@gmail.com>
2023-05-15 14:07:19 +0900

    modify

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -174,25 +174,26 @@ packer.startup(function(use)
     }
 
     -- use {
-    --     "nvim-neorg/neorg",
-    --     config = function()
-    --         require('neorg').setup {
-    --             load = {
-    --                 ["core.defaults"] = {}, -- Loads default behaviour
-    --                 ["core.concealer"] = {}, -- Adds pretty icons to your documents
-    --                 ["core.dirman"] = { -- Manages Neorg workspaces
-    --                     config = {
-    --                         -- workspaces = {
-    --                         --     notes = "~/notes",
-    --                         -- },
-    --                     },
+    -- "nvim-neorg/neorg",
+    -- -- tag = "*",
+    -- ft = "norg",
+    -- after = "nvim-treesitter", -- You may want to specify Telescope here as well
+    -- config = function()
+    --     require('neorg').setup {
+    --         load = {
+    --             ["core.defaults"] = {}, -- Loads default behaviour
+    --             ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    --             ["core.dirman"] = { -- Manages Neorg workspaces
+    --                 config = {
+    --                     -- workspaces = {
+    --                     --     notes = "~/notes",
+    --                     -- },
     --                 },
     --             },
-    --         }
-    --     end,
-    --     run = ":Neorg sync-parsers",
-    --     requires = "nvim-lua/plenary.nvim",
-    -- }
+    --         },
+    --     }
+    -- end
+}
 
 end)
 
