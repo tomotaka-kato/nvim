6a73877bdc87c0f7dbec4533f8b27965fd1d3a22
Kato Tomotaka <tomotaka.katou.dw@fujifilm.com>
2023-07-03 04:10:26 +0000

    lua-lineの設定を復活

--- after/plugin/lsp_lines.rc.lua
+++ after/plugin/lsp_lines.rc.lua
@@ -1,10 +1,9 @@
-return  -- いったん無効にするためにreturnする
--- local lsp_lines = require('lsp_lines')
---
--- lsp_lines.setup()
---
--- vim.diagnostic.config({
---   virtual_text = false,
--- })
---
--- vim.keymap.set('n', '<Leader>l', lsp_lines.toggle, { desc = 'Toggle lsp_lines' })
+local lsp_lines = require('lsp_lines')
+
+lsp_lines.setup()
+
+vim.diagnostic.config({
+  virtual_text = false,
+})
+
+vim.keymap.set('n', '<Leader>l', lsp_lines.toggle, { desc = 'Toggle lsp_lines' })
