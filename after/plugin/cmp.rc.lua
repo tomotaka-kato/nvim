0b81fdd2ff8fcf050ff5a0de74535eea9b947126
tomo <tomotaka.k612@gmail.com>
2023-07-11 19:36:36 +0900

    update

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -42,12 +42,11 @@ cmp.setup({
 })
 
 cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = "nvim_lsp_document_symbol" },
-        -- { name = "cmdline_history" },
-        { name = "buffer" },
-    }, {}),
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "nvim_lsp_document_symbol" },
+    { name = 'buffer'}
+  }
 })
 
 cmp.setup.cmdline(':', {
