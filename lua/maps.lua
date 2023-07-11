847e9efc2987e7a7845b766801fef15e9cfec0d6
tomo <tomotaka.k612@gmail.com>
2023-07-11 18:38:33 +0900

    update

--- lua/maps.lua
+++ lua/maps.lua
@@ -11,13 +11,13 @@ keymap.set('n', '<C-h>', '<C-w>h', noramap)
 keymap.set('n', '<C-k>', '<C-w>k', noramap)
 keymap.set('n', '<C-l>', '<C-w>l', noramap)
 
--- ターミナルのインサートモードから抜けるコマンドを変更
-keymap.set('t', '<C-t>', '<C-\\><C-n>', noramap)
--- ターミナルの起動コマンド
-keymap.set('n', '<C-t>s', '<cmd>botright new<CR><cmd>terminal<CR>', noramap)
-keymap.set('n', '<C-t>v', '<cmd>rightbelow vnew<CR><cmd>terminal<CR>', noramap)
-keymap.set('n', '<C-t>n', '<cmd>tabe<CR><cmd>terminal<CR>', noramap)
-keymap.set('n', '<C-t>g', '<cmd>tabe<CR><cmd>terminal<CR>lazygit<CR>', noramap)
+-- -- ターミナルのインサートモードから抜けるコマンドを変更
+-- keymap.set('t', '<C-t>', '<C-\\><C-n>', noramap)
+-- -- ターミナルの起動コマンド
+-- keymap.set('n', '<C-t>s', '<cmd>botright new<CR><cmd>terminal<CR>', noramap)
+-- keymap.set('n', '<C-t>v', '<cmd>rightbelow vnew<CR><cmd>terminal<CR>', noramap)
+-- keymap.set('n', '<C-t>n', '<cmd>tabe<CR><cmd>terminal<CR>', noramap)
+-- keymap.set('n', '<C-t>g', '<cmd>tabe<CR><cmd>terminal<CR>lazygit<CR>', noramap)
 
 -- Telescope
 keymap.set('n', '<C-p>', '<cmd>Telescope find_files<CR>', noramap)
