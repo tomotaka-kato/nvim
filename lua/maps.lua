7cb30e7226d9fbe339a5cef7b2dfeb073728b0ae
tomo <tomotaka.k612@gmail.com>
2023-08-04 18:18:34 +0900

    update

--- lua/maps.lua
+++ lua/maps.lua
@@ -5,8 +5,9 @@ vim.g.mapleader = " "
 
 local noramap = { noremap = true }
 
--- 0を^にする
+-- 0と^を入れ替える
 keymap.set('n', '0', '^', noramap)
+keymap.set('n', '^', '0', noramap)
 
 -- window移動
 keymap.set('n', '<C-j>', '<C-w>j', noramap)
