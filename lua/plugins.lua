94e9aa3b2a7099ef1e61db988e93db746cda7864
tomo <tomotaka.k612@gmail.com>
2024-04-15 18:59:40 +0900

    fix copilot chat plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -178,31 +178,17 @@ return {
   { 'rafamadriz/friendly-snippets', dependencies = { 'hrsh7th/nvim-cmp' } } ,
   { "zbirenbaum/copilot.lua", lazy = true, event = 'InsertEnter' },
   {
-    "jellydn/CopilotChat.nvim",
-    -- 以下のコマンドで依存関係を入れないとarchでは動かなかった
-    -- pip install python-dotenv requests pynvim==0.5.0 prompt-toolkit
-    opts = {
-      mode = "split",
-      prompts = {
-        Explain = "Explain how it works.",
-        Review = "Review the following code and provide concise suggestions.",
-        Tests = "Briefly explain how the selected code works, then generate unit tests.",
-        Refactor = "Refactor the code to improve clarity and readability.",
-      },
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
-    build = function()
-      vim.defer_fn(function()
-        vim.cmd("UpdateRemotePlugins")
-        vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
-      end, 3000)
-    end,
-    event = "VeryLazy",
-    keys = {
-      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
-      { "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
-      { "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
-    }
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
   { 'windwp/nvim-autopairs', event = "InsertEnter" },
   { 'windwp/nvim-ts-autotag', event = "InsertEnter" },
