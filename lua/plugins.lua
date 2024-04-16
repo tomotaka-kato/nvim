0cd35f7563ad408096db85dd0e703e315aaa0ccf
tomo <tomotaka.k612@gmail.com>
2024-04-16 16:09:32 +0900

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -185,9 +185,16 @@ return {
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
     opts = {
-      debug = true, -- Enable debugging
+      -- debug = true, -- Enable debugging
       -- See Configuration section for rest
+        mappings = {
+          reset = {
+            normal ='<leader><C-l>',
+            insert = '<C-l>'
+          },
+        },
     },
+
     -- See Commands section for default commands if you want to lazy load on them
   },
   { 'windwp/nvim-autopairs', event = "InsertEnter" },
