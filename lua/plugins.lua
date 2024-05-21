593e239785a4bce2cf0e37f52da429365ec34d8a
tomo <tomotaka.k612@gmail.com>
2024-05-21 14:46:14 +0900

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -203,7 +203,7 @@ return {
   -- [begin] ファジーファインダー
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
+    tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   -- [end] ファジーファインダー
