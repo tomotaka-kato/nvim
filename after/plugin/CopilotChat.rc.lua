d5cd4e512ddfdf506af384f995e975ffac5e2769
tomo <tomotaka.k612@gmail.com>
2024-06-06 13:21:36 +0900

    update

--- after/plugin/CopilotChat.rc.lua
+++ after/plugin/CopilotChat.rc.lua
@@ -13,9 +13,24 @@ chat.setup({
 	},
 })
 
+-- バッファを選択して CopilotChat を開く
 function CopilotChatBuffer()
 	require("CopilotChat").open({ selection = require("CopilotChat.select").buffer })
 end
 
-vim.api.nvim_set_keymap("n", "<leader>ccb<cr>", "<cmd>lua CopilotChatBuffer()<cr>", { noremap = true, silent = true })
+-- telescope を使ってアクションプロンプトを表示する
+function ShowCopilotChatActionPrompt()
+	local actions = require("CopilotChat.actions")
+	require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+end
+
+-- キーマッピング
+-- <C-c> で CopilotChat を開く
 vim.api.nvim_set_keymap("n", "<C-c>", "<cmd>lua CopilotChatBuffer()<cr>", { noremap = true, silent = true })
+-- <leader>ccp (Copilot Chat Prompt の略) でアクションプロンプトを表示する
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader><C-p>",
+	"<cmd>lua ShowCopilotChatActionPrompt()<cr>",
+	{ noremap = true, silent = true }
+)
