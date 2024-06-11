718650ccf95e2eda9407c4bb2a2fe4e84c5c6f60
tomo <tomotaka.k612@gmail.com>
2024-06-11 12:18:39 +0900

    update

--- lua/maps.lua
+++ lua/maps.lua
@@ -7,14 +7,14 @@ local noramap = { noremap = true }
 
 -- 0と^を入れ替える
 -- 一番左まで画面をスクロールしたいので、一度0を実施してから^を行う
-keymap.set('n', '0', '0^', noramap)
-keymap.set('n', '^', '0', noramap)
+keymap.set("n", "0", "0^", noramap)
+keymap.set("n", "^", "0", noramap)
 
 -- window移動
-keymap.set('n', '<C-j>', '<C-w>j', noramap)
-keymap.set('n', '<C-h>', '<C-w>h', noramap)
-keymap.set('n', '<C-k>', '<C-w>k', noramap)
-keymap.set('n', '<C-l>', '<C-w>l', noramap)
+keymap.set("n", "<C-j>", "<C-w>j", noramap)
+keymap.set("n", "<C-h>", "<C-w>h", noramap)
+keymap.set("n", "<C-k>", "<C-w>k", noramap)
+keymap.set("n", "<C-l>", "<C-w>l", noramap)
 
 -- -- ターミナルのインサートモードから抜けるコマンドを変更
 -- keymap.set('t', '<C-t>', '<C-\\><C-n>', noramap)
@@ -25,10 +25,10 @@ keymap.set('n', '<C-l>', '<C-w>l', noramap)
 -- keymap.set('n', '<C-t>g', '<cmd>tabe<CR><cmd>terminal<CR>lazygit<CR>', noramap)
 
 -- Telescope
-keymap.set('n', '<C-p>', '<cmd>Telescope find_files<CR>', noramap)
-keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', noramap)
-keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', noramap)
-keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<CR>', noramap)
+keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>", noramap)
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", noramap)
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", noramap)
+keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<CR>", noramap)
 
 -- LuaSnip
 -- インサートモードの時にTabで次の個所にジャンプする。nvimの記載方法でうまく書けなかった。。。
