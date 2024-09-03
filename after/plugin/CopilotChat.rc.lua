855caf8d80580deeee5912e19da5a3bac1344fad
tomo <tomotaka.k612@gmail.com>
2024-09-03 14:38:58 +0900

    設定変更

--- after/plugin/CopilotChat.rc.lua
+++ after/plugin/CopilotChat.rc.lua
@@ -4,9 +4,39 @@ if not status then
 	return
 end
 
+local prompts = require("CopilotChat.prompts")
 local select = require("CopilotChat.select")
 
 chat.setup({
+	debug = false, -- Enable debug logging
+	proxy = nil, -- [protocol://]host[:port] Use this proxy
+	allow_insecure = false, -- Allow insecure server connections
+
+	system_prompt = prompts.COPILOT_INSTRUCTIONS, -- System prompt to use
+	model = "gpt-4o", -- GPT model to use, 'gpt-3.5-turbo', 'gpt-4', or 'gpt-4o'
+	temperature = 0.1, -- GPT temperature
+
+	question_header = "## User ", -- Header to use for user questions
+	answer_header = "## Copilot ", -- Header to use for AI answers
+	error_header = "## Error ", -- Header to use for errors
+	separator = "───", -- Separator to use in chat
+
+	show_folds = true, -- Shows folds for sections in chat
+	show_help = true, -- Shows help message as virtual lines when waiting for user input
+	auto_follow_cursor = true, -- Auto-follow cursor in chat
+	auto_insert_mode = false, -- Automatically enter insert mode when opening window and on new prompt
+	insert_at_end = false, -- Move cursor to end of buffer when inserting text
+	clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
+	highlight_selection = true, -- Highlight selection in the source buffer when in the chat window
+
+	context = nil, -- Default context to use, 'buffers', 'buffer' or none (can be specified manually in prompt via @).
+	history_path = vim.fn.stdpath("data") .. "/copilotchat_history", -- Default path to stored history
+	callback = nil, -- Callback to use when ask response is received
+
+	-- default selection (visual or line)
+	selection = function(source)
+		return select.visual(source) or select.line(source)
+	end,
 	mappings = {
 		reset = {
 			normal = "<leader><C-l>",
@@ -75,6 +105,7 @@ chat.setup({
 		CommitStagedJa = {
 			prompt = "Write commit message for the change with commitizen convention in Japanese. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.",
 			selection = function(source)
+				print("source", source)
 				return select.gitdiff(source, true)
 			end,
 		},
