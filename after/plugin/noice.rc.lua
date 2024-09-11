72699c075035382337cb9c014380b8c9e313703d
tomo <tomotaka.k612@gmail.com>
2024-09-12 03:05:00 +0900

    update

--- after/plugin/noice.rc.lua
+++ after/plugin/noice.rc.lua
@@ -16,8 +16,8 @@ noice.setup({
 			-- opts: any options passed to the view
 			-- icon_hl_group: optional hl_group for the icon
 			-- title: set to anything or empty string to hide
-			cmdline = { pattern = "^:", icon = "", lang = "vim", concal = false },
-			search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+			cmdline = { pattern = "^:", icon = "", lang = "vim", conceal = false },
+			search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex", conceal = false },
 			search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
 			filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
 			lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
